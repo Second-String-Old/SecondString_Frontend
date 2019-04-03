54c2cadca283e5ad71e761f5cf245aba38b82dcf
cwilliamson226 <colin.williamson@narrativ.com>
2019-04-03 02:10:31 -0400

    Created QB Table hooked up to backend

--- secondString/routes/index.js
+++ secondString/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
+const https = require('https')
 var router = express.Router();
+
+// Sets options to QB route on flask backend
+const options = {
+  hostname: 'flask-ss-heroku.herokuapp.com',
+  path: '/players/QB/',
+  method: 'GET'
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Second String' });
+  // Requests QB data
+  const qbReq = https.request(options, (qbRes) => {
+    // On response
+    qbRes.on('data', (d) => {
+      // Parses GET response into JSON object data
+      var data = JSON.parse(d.toString());
+      // Renders stat page with data object
+      res.render('index', { title: 'Second String', data: data });
+    })
+  })
+  
+  qbReq.on('error', (error) => {
+    console.error(error)
+  })
+  
+  qbReq.end()
 });
 
 module.exports = router;
